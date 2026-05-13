6b77473ca46aecebe7931b4633b4375d14869a4b
Enderboy <enderboy5062@gmail.com>
2026-05-13 17:15:02 +0300

    Multi-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import NavBar from "./components/NavBar.jsx";
 import ModrinthModCard from "./components/ModrinthModCard.jsx";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import AboutMe from "./AboutMe.jsx";
 
 function App() {
   return (<>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<AboutMe />} />
+      </Routes>
+    </Router>
+
     <NavBar isHome={true}/>
     <br/>
     <div className="mod-card-list">
