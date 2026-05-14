71e345b55fdf8a0042c04e638feac5fbbdaea245
Enderboy <enderboy5062@gmail.com>
2026-05-14 15:01:13 +0300

    status

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,7 +8,17 @@ function Home() {
         <div className="mod-card-list">
             <ModrinthModCard modId="netherandend"/>
             <br/>
+            <ModrinthModCard modId="ironandsteel" status="discontinued"/>
+            <br/>
             <ModrinthModCard modId="enderlib" isOpenSource={true} githubRepoName="Enderlib" hasWiki={true}/>
+            <br/>
+            <ModrinthModCard modId="tricksandtraps" status="discontinued"/>
+            <br/>
+            <ModrinthModCard modId="lootbundles" status="discontinued"/>
+            <br/>
+            <ModrinthModCard modId="stains-and-colors" status="unknown"/>
+            <br/>
+            <ModrinthModCard modId="allay-vex-conversion"/>
             <br/>
             <ModrinthModCard modId="bellum"/>
         </div>
