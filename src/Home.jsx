9e6e965f7166a56f1f3a1108e391d9bd1f99e0f6
Enderboy <enderboy5062@gmail.com>
2026-05-14 14:29:40 +0300

    status

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
         <NavBar isHome={true}/>
         <br/>
         <div className="mod-card-list">
-            <ModrinthModCard modId="netherandend" status="redesigning"/>
+            <ModrinthModCard modId="netherandend"/>
             <br/>
             <ModrinthModCard modId="enderlib" isOpenSource={true} githubRepoName="Enderlib" hasWiki={true}/>
             <br/>
