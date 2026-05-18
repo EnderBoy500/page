18b3165cf82865187f5a6161777b5fb6b42a8a16
Enderboy <enderboy5062@gmail.com>
2026-05-18 13:38:31 +0300

    Fixing Mobile

--- src/components/ModrinthModCard.jsx
+++ src/components/ModrinthModCard.jsx
@@ -77,8 +77,6 @@ function ModrinthModCard({modId = "", status = "supported", isOpenSource = false
         if (window.screen.width <= 768) {
             const scale = {
                 scale: "80%",
-                position: "absolute",
-                left: "10px",
             }
 
             return (
